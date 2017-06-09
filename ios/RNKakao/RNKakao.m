0f78d054f018af5b28b68a584a04c3fa8136c6a9
Lee Kanghyeon <kang926@gmail.com>
2017-06-09 17:59:07 +0900

    수정

--- ios/RNKakao/RNKakao.m
+++ ios/RNKakao/RNKakao.m
@@ -53,10 +53,58 @@ RCT_REMAP_METHOD(login,
                 reject(@"RNKakao", @"login canceled", nil);
                 return;
             }
-        } authParams:nil authTypes:(authTypes != nil) ? authTypes : @[@(KOAuthTypeTalk), @(KOAuthTypeStory), @(KOAuthTypeAccount)]];
+        } authParams:nil authTypes:(authTypes != nil) ? authTypes : @[@(KOAuthTypeTalk), @(KOAuthTypeAccount)]];
     });
 }
 
+/**
+ Get userInfo
+ */
+RCT_REMAP_METHOD(userInfo,
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [self userInfoRequestResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject];
+}
+
+- (void) userInfoRequestResolve:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject {
+    [KOSessionTask meTaskWithCompletionHandler:^(KOUser* result, NSError *error) {
+        if (result) {
+            // success
+            
+            NSNumber *id = result.ID;
+            NSString *nickName = [result propertyForKey:KOUserNicknamePropertyKey];
+            NSString *email = [result propertyForKey:KOUserEmailPropertyKey];
+            NSString *profileImage = [result propertyForKey:KOUserProfileImagePropertyKey];
+            NSString *profileImageThumnail = [result propertyForKey:KOUserThumbnailImagePropertyKey];
+            
+            NSMutableDictionary *userInfo = [NSMutableDictionary dictionary];
+            [userInfo setValue:id forKey:@"id"];
+            [userInfo setValue:[KOSession sharedSession].accessToken forKey:@"accessToken"];
+            
+            if (nickName != nil) {
+                [userInfo setValue:nickName forKey:@"nickName"];
+            }
+            
+            if (email != nil) {
+                [userInfo setValue:email forKey:@"email"];
+            }
+            
+            if (profileImage != nil) {
+                [userInfo setValue:profileImage forKey:@"profileImage"];
+            }
+            
+            if (profileImageThumnail != nil) {
+                [userInfo setValue:profileImageThumnail forKey:@"profileImageThumnail"];
+            }
+            
+            resolve(userInfo);
+        } else {
+            // failed
+            reject(@"RNKakao", @"userInfo error", error);
+        }
+    }];
+}
 
 
 RCT_REMAP_METHOD(logout,
